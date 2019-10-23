a1788614a839b36307830c97142903a4d7ea55f6
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-23 22:55:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# My Code here....
+def map_to_negativize(source_array)
+  i = 0 
+  while i < source_array.length 
+  arr << -ABS(source_array[i])
+  i++
+end
+return arr 
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end 
+
+def map_to_double(source_array)
+  i = 0
+  arr = []
+  while i < source_array.length 
+  arr << (source_array[i] * 2)
+  i++
+end
+return arr 
+end
