8dafc2c9c971e15d059f721586666cb32026b4b9
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-23 23:04:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,7 +33,7 @@ end
 
 def reduce_to_total(source_array, starting_point)
   sum = 0
-  i = starting point - 1
+  i = starting_point - 1
   while i < source_array.length do
     
     sum += source_array[i]
