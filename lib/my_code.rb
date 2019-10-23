1810f94be1f0f04dafda77e1cf333b2768b1f550
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-23 23:11:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,9 +43,17 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.each{ |n| = "true"
-  puts source_array
-end
+  i = 0 
+  while i < source_array.length do 
+    source_array[i] = "true"
+    i += 1 
+  end 
+  puts source_array 
+end 
+
+
+
+
     
 
 
