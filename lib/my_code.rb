82d1a2483a05112804527a1f3d24ad3b90519e6d
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-23 23:00:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,3 +21,19 @@ end
 return arr 
 end
 
+def map_to_square(source_array)
+  i = 0
+  arr = []
+  while i < source_array.length 
+  arr << (source_array[i] * source_array[i])
+  i += 1
+end
+return arr 
+end
+
+def reduce_to_total(source_array, starting_point)
+  
+  
+  source_array[(starting_point - 1)]
+
+
